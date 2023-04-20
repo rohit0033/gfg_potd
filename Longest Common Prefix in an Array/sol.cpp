25fff2d85f40b1a6e03a6aa8084a7cb8b7d3e925
Rohit Sharma <112507390+rohit0033@users.noreply.github.com>
2023-04-20 22:47:52 +0530

    Update sol.cpp

--- Longest Common Prefix in an Array/sol.cpp
+++ Longest Common Prefix in an Array/sol.cpp
@@ -52,3 +52,8 @@ int main()
 // Contributed By: Pranay Bansal
 
 // } Driver Code Ends
+
+// The approach heavily depends on the idea of sorting. If we sort a array of strings, the strings get sorted lexographically, 
+// which means the strings are sorted according to their character occurrence, so, the first string should be present in all the
+// strings of the sorted array and since the last string in array is very different from every other string and hence it was placed
+// last, so if we find the occurrence of our first string in the last string , we will get the common prefix. that's the idea
