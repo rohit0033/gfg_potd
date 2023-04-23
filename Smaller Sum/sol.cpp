c78ef181e5c067f29b8d20c98f783def0da1e5a4
Rohit Sharma <112507390+rohit0033@users.noreply.github.com>
2023-04-23 19:10:58 +0530

    Create sol.cpp

---
+++ Smaller Sum/sol.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+    vector<long long> smallerSum(int n,vector<int> &arr){
+        // Code here
+         vector<int>newArr=arr;
+        
+        // sort 
+        sort(newArr.begin(),newArr.end());
+        
+        
+        unordered_map<long long, long long>mp;
+        long long sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            // check 
+            if(mp.find(newArr[i])==mp.end())
+                mp[newArr[i]]=sum;
+            
+            // netx
+            sum+=newArr[i];
+        }
+        
+        vector<long long>ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(mp[arr[i]]);
+        }
+        return ans;
+    
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob; 
+        vector<long long> ans=ob.smallerSum(n,arr);
+        for(int i=0;i<n;i++){
+            if(i!=n-1){
+                cout<<ans[i]<<" ";
+            }
+            else{
+                cout<<ans[i]<<endl;
+            }
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
